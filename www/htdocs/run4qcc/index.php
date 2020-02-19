130132025865c290a6ed6f126a1dfc933ff6873c
Theo Chino <theo@outrageddems.nyc>
2020-02-19 13:55:36 -0500

    February 19, 2020 update message

--- www/htdocs/run4qcc/index.php
+++ www/htdocs/run4qcc/index.php
@@ -2,21 +2,27 @@
 <IMG SRC="QCC4ALL.jpg" width=100>
 
 <P>
-<B>Update:</B> February 6<SUP>th</SUP>, 2020 at 3:18 am.<BR>
+<B>Update:</B> February 19<SUP>th</SUP>, 2020 at 1:48 pm.<BR>
 <FONT SIZE=+2>	
 	<B>
-		This page was supposed to be accessible on February 6<SUP>th</SUP>, 2020 but we are running behind schedule.
+		This page was supposed to be accessible on February 6<SUP>th</SUP>, 2020 but we are running way behind schedule.
 	</B>
-</FONT>
 <BR>
 
 <P>
-	<UL>
-	<IMG SRC="WalkSheet.png" HEIGHT=600>
-	</UL>
-</P>
 
-<FONT SIZE=+3>
+<FOnT COLOR=BLUE>
+You can register on the RepMyBlock website at <A HREF="https://repmyblock.nyc/register/">https://repmyblock.nyc/register</A> 
+and fill in the Profile. 
+<BR>
+<BR>
+In the BIO section write dow the Assembly District and the Election District you are representing and we'll email you the walk sheet.
+</FOnT>
+
+</P>
+</FONT>
+
+<FONT SIZE=+1>
 	
 Meanwhile you can register friends to run for County Committee on 
 their page:
