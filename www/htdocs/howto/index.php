2cbc92554e2c28823aae014359279f69e72463c2
Theo Chino <theo@outrageddems.nyc>
2020-03-11 11:47:12 -0400

    Added some information to the HOWTO

--- www/htdocs/howto/index.php
+++ www/htdocs/howto/index.php
@@ -2,7 +2,7 @@
 	$BigMenu = "howto";
 	if ( ! empty ($k)) { $MenuLogin = "logged"; }
 	
-	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../statlib/Config/Vars.php";
 	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/funcs/general.php";
 	include $_SERVER["DOCUMENT_ROOT"] . "/headers/headers.php"; 	
 
@@ -17,10 +17,25 @@
 		</P>
 		
 		<P CLASS="f60">
-			<B>
-				How to file the documents.
-			</B>
+			<B>How to file the documents.</B>
 		</P>
+
+		<P CLASS="f40">
+			The filing period will begin on <B>March 30<SUP>th</SUP></B> and end at midnight on April 2<SUP>nd</SUP>. 
+			<B>You do not need to drop your petitions before March 30<SUP>th</SUP>.</B>
+		<BR>
+			Before you drop your petitions, you will need to apply for a Pre Assigned ID Number by 
+			<B><A HREF="<?= $FrontEndPDF ?>/NYS/NYC/CRU_PreFile" TARGET="CRU">completing the Petition Pre Assigned Identification Number Application</A></B>
+			and mailing it to the Candidate Record Unit at the Board of Election.
+		<BR>
+		 <I>(If you have used the RepMyBlock system, <A HREF="/login"><B>log into your account</B></A> to download a custom pre-filed one.)</I>
+		</P>
+
+		
+		<P>
+			<IMG SRC="BOECalendar.png">
+		</P>
+
 
 	</DIV>
 	</DIV>
