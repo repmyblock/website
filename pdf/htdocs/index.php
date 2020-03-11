8264698006b2717641b82d9074def010727c285b
Theo Chino <theo@outrageddems.nyc>
2020-03-11 11:17:37 -0400

    Added

--- pdf/htdocs/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-	require $_SERVER["DOCUMENT_ROOT"] . "/../statlib/Config/Vars.php";
-	header("Location: " . $FrontEndPDF . "/multipetitions/");
-?>
---
+++ www/htdocs/404/index.php
@@ -0,0 +1,19 @@
+<?php
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/funcs/general.php";
+	include $_SERVER["DOCUMENT_ROOT"] . "/headers/headers.php"; 	
+?>
+
+<P><BR><BR></P>
+
+<CENTER><h2>404 - Page not found</h2>
+
+
+<P><BR>
+	This page used to exist but doesn't anymore.<BR>Please email us at 
+	info at the domain name for this website so we can check why the 
+	information is missing.</P>	
+</CENTER>
+<?php
+	include $_SERVER["DOCUMENT_ROOT"] . "/headers/footer.php"; 
+?>
