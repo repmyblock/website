627fe118efa51dbf0f8a0d6b538bde0c284ec5e0
Theo Chino <theo@outrageddems.nyc>
2020-10-19 13:17:33 -0400

    Removed a few bugs and the call to the databases that are not necessary

--- www/htdocs/lgd/voters.php
+++ www/htdocs/lgd/voters.php
@@ -3,42 +3,11 @@
 	$Menu = "voters";
 	$BigMenu = "represent";	
 	
-  require $_SERVER["DOCUMENT_ROOT"] . "/../statlib/Config/Vars.php";	
 	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
 	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/funcs/general.php";
-	// require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/db/db_voterlist.php";  
-	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/db/db_repmyblock.php";  
+
   if (empty ($URIEncryptedString["SystemUser_ID"])) { goto_signoff(); }
 
-	if ( empty ($URIEncryptedString["MenuDescription"])) { $MenuDescription = "District Not Defined";}	
-	$Party = NewYork_PrintParty($URIEncryptedString["UserParty"]);
-
-	$rmb = new RepMyBlock();	
-	$result = $rmb->GetPetitionsForCandidate($DatedFiles, 0, $URIEncryptedString["SystemUser_ID"]);
-	
-	if ( ! empty ($result)) {
-		foreach ($result as $var) {
-			if ( ! empty ($var)) {
-
-				$MyAddressToUse = $var["Raw_Voter_ResHouseNumber"] . " " . 
-													$var["Raw_Voter_ResStreetName"];
-
-				if ( empty ($Counter[$MyAddressToUse] )) {
-					$Counter[$MyAddressToUse] = 0;
-				}
-				
-				$Electors[$MyAddressToUse][$Counter[$MyAddressToUse]]["Petition_ID"] = $var["Candidate_ID"];
-				$Electors[$MyAddressToUse][$Counter[$MyAddressToUse]]["Elector_ID"] = $var["VotersIndexes_UniqNYSVoterID"];
-				$Electors[$MyAddressToUse][$Counter[$MyAddressToUse]]["Elector_FullName"] = $var["CandidatePetition_VoterFullName"];
-				$Electors[$MyAddressToUse][$Counter[$MyAddressToUse]]["Elector_Address"] = "Apt " . $var["Raw_Voter_ResApartment"];
-				$Electors[$MyAddressToUse][$Counter[$MyAddressToUse]]["Full_Elector_Address"] = $var["Raw_Voter_ResHouseNumber"] . " " . $var["Raw_Voter_ResStreetName"];
-			}			
-			
-			$Counter[$MyAddressToUse]++;
-		}	
-	}
-
-			
 	include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php";
 	if ($MobileDisplay == true) { $Cols = "col-12"; } else { $Cols = "col-9"; }
 ?>
@@ -64,9 +33,9 @@
 			include $_SERVER["DOCUMENT_ROOT"] . "/common/warning_voterinfo.php";
 		} 
 	?>
-	
-	<A HREF="/lgd/<?= $k ?>/voterquery">Query for a voter</A><BR>
-	<A HREF="/lgd/<?= $k ?>/voterlist">List of voters in district</A>
+
+	<A HREF="/lgd/<?= $k ?>/voterlist">List of voters in district</A><BR>
+	<A HREF="/lgd/<?= $k ?>/voterquery">Query for a voter</A>
 			
 
 
