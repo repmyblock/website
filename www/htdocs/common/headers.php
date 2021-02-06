8f06e8f8438f00b58c253314352695039b26e134
Theo Chino <theo@outrageddems.nyc>
2021-02-06 01:47:56 -0500

    Fixed description for ticket #4 https://trac.repmyblock.nyc/ticket/4

--- www/htdocs/common/headers.php
+++ www/htdocs/common/headers.php
@@ -15,7 +15,7 @@
 	<HEAD>
 		<META charset="UTF-8">
 		<meta name="twitter:card" content="summary_large_image" />
-		<meta name="twitter:description" content="Registrations for the 2020 county committee cycle in the Bronx, Queens and Brooklyn have started. Registration for the 2021 county committee cycle in Manhattan and Staten Island have started. Get your nominating petition kit here! HOW DOES IT WORKS The County Committee is the most basic committee of the Democratic Party; its its backbone. The &hellip; Continue reading Rep My Block &rarr;" />
+		<meta name="twitter:description" content="Registrations for the 2021 Citywide Republican county committee and 2021 Manhattan and Staten Island Democratic county committee have started. Get your nominating petition kit here! The County Committee is the most basic committee of the Democratic and Republican Parties; its their backbone. The &hellip; Continue reading Rep My Block &rarr;" />
 		<meta name="twitter:title" content="Rep My Block - Rep My Block" />
 		<meta name="twitter:site" content="@RepMyBlock" />
 		<meta name="twitter:image" content="https://static.repmyblock.nyc/pics/paste/PoliticalMachineKeepEngagementLow.jpg" />
