2f5aa1c3088f20cad9a7c3eadd77f022affbe94c
Theo Chino <theo@outrageddems.nyc>
2021-02-25 18:17:48 -0500

    Added the training videos

---
+++ www/htdocs/exp/training.php
@@ -0,0 +1,83 @@
+<?php
+//	$imgtoshow = "/brand/RunWithMe/RunWithMe.png";
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
+	include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php"; 
+	if ( $MobileDisplay == true ) { $TypeEmail = "email"; $TypeUsername = "username";
+	} else { $TypeEmail = "text"; $TypeUsername = "text"; }
+?>
+<DIV class="main">
+	<DIV CLASS="right f80">Running for Office Training Material</DIV>
+
+
+<h4>Zoom with Paperboy hosted by el Chino!</h4>
+
+
+
+<P>
+	<B>Episode #1:</B> Sal Albanese explains the corruption of the New York system.
+</P>
+
+
+<P>
+	<UL>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/UwyfucMSzDY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</UL>
+</P>
+
+
+
+<P>
+	<B>Episode #2:</B> Badrun Khan explains the gruling work that goes into petitioning.
+</P>
+
+<P>
+	<UL>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/lc_sgWGxZWo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</UL>
+</P>
+
+
+
+<P>
+	<B>Episode #3:</B> Ben Yee explains the work of the Activist.
+</P>
+
+<P>
+	<UL>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/NU1hVBaXTNo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</UL>
+</P>
+
+<P>
+	<B>Episode #4:</B> Vittoria Fariello explain the expectation of being elected as a Party Official.
+</P>
+
+<P>
+	<UL>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/GsQ7YtaunCE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</UL>
+</P>
+
+<P>
+	<B>Episode #5:</B> Jared Rich goes over the inportance of the access of the local press in being considered a viable candidate.
+</P>
+
+<P>
+	<UL>
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/AO7iBH0xgkA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</UL>
+</P>
+
+			
+		
+		
+		
+			<P CLASS="f60">
+				Rep My Block is provided Free of Charge to any candidate that wishes to integrate 
+				it's services.
+			</P>
+			
+	</FORM>
+</DIV>
+		
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/common/footer.php"; ?>
