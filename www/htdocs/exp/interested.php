bd25c45b6d4d3ef8d76c10d9a3a2737fa5c289ca
Theo Chino <theo@outrageddems.nyc>
2021-03-01 04:13:05 -0500

    Fixed the footer to add the BUG menu without breaking the site. https://trac.repmyblock.nyc/ticket/23

--- www/htdocs/exp/interested.php
+++ www/htdocs/exp/interested.php
@@ -3,7 +3,6 @@
 	if ( ! empty ($k)) { $MenuLogin = "logged"; }
 	
 	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
-	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/funcs/general.php";
 	include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php"; 	
 
 	/* User is logged */
