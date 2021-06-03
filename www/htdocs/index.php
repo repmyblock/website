457da891f3c728e55c0d63d7b7c796f50c005707
Theo Chino <theo@outrageddems.nyc>
2021-06-03 18:27:27 -0400

    Changed text to reflect the Print Error.

--- www/htdocs/index.php
+++ www/htdocs/index.php
@@ -13,6 +13,37 @@
 <div class="main">
 	<CENTER>
 		
+	<P CLASS="BckGrndElement">A PRINT ERROR IN THE NEW YORK VOTING GUIDE</P>
+
+	<P>
+		My name is Theo Chino, and I am the Co-Founder of this Non-Partisant website. Due to a print error in the New York City Voter Guide, this website is referenced instead of my campaign website. 
+		<B>If you are looking for my campaign website, it's <A HREF="https://pubadvocate.nyc">https://pubadvocate.nyc</A></B>. 
+	</P>
+	
+	<P>
+		This website is Non Partisant. It's features instructions for the four recognized parties in New York State; Democratic, Republican, Conservative and Working Families.
+	</P>
+	
+	<P>
+		<B>However, if you are interested in participating in the local democracy on your block, Rep My Block is for you.</B>  
+		I suggest you read <A HREF="http://www.newyorktrue.com/photo-gallery-manhattan-dems-organize">John Kenny's blog</A> regarding the annual meeting where we chose the party chairman.
+	</P>
+
+
+	<P CLASS="BckGrndElement">ZOOM WITH PAPERBOY LOVE PRINCE</P>
+
+	<P>
+		<B>Sal Albanese</B>, <B>Badrun Khan</B>, <B>Ben Yee</B>, <B>Vittoria Fariello</B>, and <B>Jared Rich</B> discuss with 
+		Paperboy Love Prince the weaponization of the electoral process in five one-hour candids chats. These videos were 
+		recorded while Paperboy Love Prince was running for congress and discovering first hand the different steps of running. 
+		Those video chats' goal is to demonstrate there is hope, but it will require every voter to participate in the process 
+		beyond showing up to the polls to vote. <B>Democracy depends on it!</B>
+	</P>
+
+	<P>
+		<P CLASS="f80 center"><A HREF="/exp/website/training">Access the video chats</A></P>
+	</P>
+		
 	<P CLASS="BckGrndElement">WHAT IS THE COUNTY COMMITTEE</P>
 
 	<P>
@@ -58,19 +89,7 @@
 	</P>
 		
 
-<P CLASS="BckGrndElement">ZOOM WITH PAPERBOY LOVE PRINCE</P>
 
-	<P>
-		<B>Sal Albanese</B>, <B>Badrun Khan</B>, <B>Ben Yee</B>, <B>Vittoria Fariello</B>, and <B>Jared Rich</B> discuss with 
-		Paperboy Love Prince the weaponization of the electoral process in five one-hour candids chats. These videos were 
-		recorded while Paperboy Love Prince was running for congress and discovering first hand the different steps of running. 
-		Those video chats' goal is to demonstrate there is hope, but it will require every voter to participate in the process 
-		beyond showing up to the polls to vote. <B>Democracy depends on it!</B>
-	</P>
-
-	<P>
-		<P CLASS="f80 center"><A HREF="/exp/website/training">Access the video chats</A></P>
-	</P>
 
 
 	
