3aaec3edea6192276b7f0addd7ecc160e6673051
Theo Chino <theo@outrageddems.nyc>
2022-01-18 17:40:17 -0500

    Changed the landing location depending of logged status when clicking on man logo. Fixed the error when logged and clicking around.Z

--- www/htdocs/common/headers.php
+++ www/htdocs/common/headers.php
@@ -6,8 +6,10 @@
 												$URIEncryptedString["SystemUser_ID"] == "TMP")) { 
 		$MenuLogin = "logged"; 
 		$middleuri = rawurlencode($k);	
+		$logourl = "/" . $k . "/lgd/summary/summary";
 	} else {
 		$middleuri = "web";
+		$logourl = "/";
 	}
 	if ( empty ($imgtoshow )) {	$imgtoshow = "/images/RepMyBlock.png"; }	
 	
@@ -60,12 +62,12 @@
   <BODY class="logged-in env-production emoji-size-boost min-width-lg page-account">
 		<DIV class="header">
 			<div class="header-left">
-			  <a href="/" class="logo"><IMG SRC="<?= $imgtoshow ?>" class="header-logo"></a>
+			  <a href="<?= $logourl ?>" class="logo"><IMG SRC="<?= $imgtoshow ?>" class="header-logo"></a>
 			</DIV>
 		  <div class="header-right">
 		  	<?php if ( $MenuLogin == "logged") { ?>
-			  	<a class="login" href="/<?= $middleuri ?>/exp/contact/contact">CONTACT</a>
-					<a href="/<?= $middleuri ?>/lgd/signoff" class="login right<?php if ($BigMenu == "profile") { echo " active"; } ?>">LOGOUT</a>
+			  	<a class="login" href="/<?= $k ?>/exp/contact/contact">CONTACT</a>
+					<a href="/<?= $k ?>/lgd/signoff" class="login right<?php if ($BigMenu == "profile") { echo " active"; } ?>">LOGOUT</a>
 				<?php } else { ?>
 			  	<a class="login" href="/<?= $middleuri ?>/exp/register/register">REGISTER</a>
 		    	<a href="/<?= $middleuri ?>/exp/login/login" class="login">LOGIN</a>
@@ -87,10 +89,11 @@
 
 <div class="navbar">
 <?php if ( $MenuLogin == "logged") { ?>
-  <a href="/<?= $middleuri ?>/lgd/profile/profile" class="right<?php if ($BigMenu == "profile") { echo " active"; } ?>">PROFILE</a>
+  <a href="/<?= $k ?>/lgd/profile/profile" class="right<?php if ($BigMenu == "profile") { echo " active"; } ?>">PROFILE</a>
 <?php /*  <a href="/<?= $middleuri ?>/exp/nominate"<?php if ($BigMenu == "nominate") { echo " class=\"active\""; } ?>>NOMINATE</a> */ ?>
-  <a href="/<?= $middleuri ?>/lgd/voters"<?php if ($BigMenu == "represent") { echo " class=\"active\""; } ?>>REPRESENT</a>
-  <a href="/<?= $middleuri ?>/exp/toplinks/about"<?php if ($BigMenu == "home") { echo " class=\"active\""; } ?>>ABOUT</a>
+<?php /*  <a href="/<?= $k ?>/lgd/voters"<?php if ($BigMenu == "represent") { echo " class=\"active\""; } ?>>REPRESENT</a> */ ?>
+  <a href="/<?= $k ?>/training/steps/torun"<?php if ($BigMenu == "represent") { echo " class=\"active\""; } ?>>REPRESENT</a>
+  <a href="/<?= $k ?>/exp/toplinks/about"<?php if ($BigMenu == "home") { echo " class=\"active\""; } ?>>ABOUT</a>
 <?php } else { ?>
  	<a  href="/<?= $middleuri ?>/exp/contact/contact" class="right<?php if ($BigMenu == "contact") { echo " active"; } ?>">CONTACT</a>
   <a href="/<?= $middleuri ?>/exp/toplinks/howto" class="right<?php if ($BigMenu == "howto") { echo " active"; } ?>">HOW TO</a>
