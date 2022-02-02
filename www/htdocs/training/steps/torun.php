ba1800d0e98afab614c8c95a55b433651f899daa
Theo Chino <theo@outrageddems.nyc>
2022-02-02 07:28:51 -0500

    Fixed the font on the list, removed the accordeons and centered the video.

--- www/htdocs/training/steps/torun.php
+++ www/htdocs/training/steps/torun.php
@@ -18,64 +18,62 @@
 			<h1 CLASS="intro">I WANT TO RUN FOR COUNTY COMMITTEE</H1>
 		</P>
 		
-		<P CLASS="f60">
-			<B>
-				The process to run for County Committee is the same as other positions in New York State. 
-				Running for President of the United States, City Council, or County Committee follow the same process. 
-			</B>
+		<P CLASS="f40">
+			The process to run for County Committee is the same as other positions 
+			in New York State. Running for President of the United States, City Council, 
+			or County Committee follow the same process. 			
 		</P>
 	</DIV>
 	
 	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
 
-	<button class="accordion2">The First Step</button>
-	<div class="panel2">
+	<H2>The First Step</H2>
 	
-		<P CLASS="f60 justify">
+		<P CLASS="f40 justify">
 			The first step is merely saying, "I am doing it." The process will not make much sense, but everything is like that to dissuade people 
 			from running. We built this website to streamline the process to remove all the hoops from running.
 		</P>
 		
-		<P CLASS="f60 justify">
+		<P CLASS="f40 justify">
 			The party, like any Non-Profit Organisation or For-Profit Corporation, has bylaws. You can find the bylaws of the various Democratic, 
 			Republican, Conservative and Working Families Parties County Committee at the Board of Election. 
 			<I>(We are working on getting the Bylaws from the other parties, and we will publish them as soon as we have them.)</I>
 		</P>
 		
 		<DIV class="videowrapper">
-		 <iframe src="https://www.youtube.com/embed/MgAY-Ipyk1Q?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-		</P>
+			<CENTER>
+				<iframe src="https://www.youtube.com/embed/MgAY-Ipyk1Q?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
+		</DIV>
 		
-
-	
 		<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
 	
-	</DIV>
 
 
-	<button class="accordion2">The Second Step</button>
-	<div class="panel2">
 
-		<P CLASS="f60 justify">
+	<H2>The Second Step</H2>
+	
+
+		<P CLASS="f40 justify">
 			You have decided to run, <B>congratulation!</B>
 			Before you start, we need to ensure that your voter registration with the board of election is correct. 
 			You will require that you are registered in an electoral party based on your convictions.
 		</P>
 		
-		<P CLASS="f60 justify">
+		<P CLASS="f40 justify">
 			If you are not registered to vote or are not registered in a party, you can print a form or go to the DMV website and register. 
 		<B>You can verify your registration automatically on the Rep My Block website.</B>
 		</P>
 		
 			
 		<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
-		</DIV>
 
 
-	<button class="accordion2">The Third Step</button>
-	<div class="panel2">
+
+	<H2>The Third Step</H2>
+	
 		
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		This step is <?= $ImportantDates["NY"]["CycleLength"] ?> days long, and in <?= $ImportantDates["NY"]["Cycle"] ?>, 
 		it will start on <?= $ImportantDates["NY"]["LongDate"]["FirstPetitionDay"] ?>, 
 		and it ends on <?= $ImportantDates["NY"]["LongDate"]["LastPetitionDay"] ?>.  
@@ -84,60 +82,66 @@
 	
 
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/cizp2jVf-Yk?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 	</DIV>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		Contrary to the presidential petitioning, won't need to stop random voter in the street but knock on your neighbors' doors. 
 		The Rep My Block website has the walk sheet that is organized by streets. 
 	</P>	
 	
 	<DIV class="videowrapper">
+			<CENTER>
 		 <iframe src="https://www.youtube.com/embed/XUGFbBCcIS4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 		</DIV>
 	
 
 	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></H2></CENTER></P>
-	</DIV>
 
 
-	<button class="accordion2">The Fourth Step</button>
-	<div class="panel2">
+
+	<H2>The Fourth Step</H2>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		Once you have collected the required numbers of signatures, you will need to bind the petitions according to the New York State Board of Elections' stringent rules.
 	</P>
 
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		Rep My Block is composed of hundreds of volunteers that have done the process. If you have any questions, a volunteer will be able to answer you put them together.
 	</P>
 
 	
 
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/9GfIm72Ksz0?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 	</DIV>
 	
 	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
 
 	
-		</DIV>
+	
 
 
-	<button class="accordion2">The Fifth Step</button>
-	<div class="panel2">
+	<H2>The Fifth Step</H2>
 	
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		This step is where you go to the Board of Election to drop your petitions between 
 		<?= $ImportantDates["NY"]["LongDate"]["FirstSubmitDay"] ?>, and <?= $ImportantDates["NY"]["LongDate"]["LastPetitionDay"] ?>. 
 
 	
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/G0yRhVGz2TM?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 	</DIV>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		Rep My Block is a community-based system that uses the power of the community. We will setup dropping points throughout the 
 		community and submit the petition for you. 
 		All you will need to do is to staple your petition together and drop them at a drop off location. 
@@ -145,95 +149,78 @@
 
 	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
 	
-		</DIV>
+		
 
 
-	<button class="accordion2">The Sixth Step</button>
-	<div class="panel2">
+	<H2>The Sixth Step</H2>
 	
-	<P CLASS="f60 justify">
+	
+	<P CLASS="f40 justify">
 		This step is the exciting step where you run for County Committee, and you remind your neighbors to vote for you on <?= $ImportantDates["NY"]["LongDate"]["PrimaryElection"] ?>.
 	</P>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		The night before, we sugest that you put some flyers around your neighborhood to remind your voters to vote for you.
 	</P>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		Rep My Block resources will have a few tools to help you create a quick flyer to put around your block and explain 
 		the Department of Sanitation rules regarding those flyers.
 	</P>
 
 	
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/WXRd6lF3Ix4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 	</DIV>
 	
 	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
-	</DIV>
 
 
-	<button class="accordion2">The Seventh Step</button>
-	<div class="panel2">
+
+	<H2>The Seventh Step</H2>
 	
-	<P CLASS="f60 justify">
+	
+	<P CLASS="f40 justify">
 		</B>Finally!</B> You spend 27 hours completing the previous six steps. <B>It is now time to attend your first meeting!</B>
 	</P>
 
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/ZyKD5H0y0KM?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-	</DIV>
+				</CENTER>
+</DIV>
 
 
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		Rep My Block will have contacted you with other County Committee members that hopefully share your vision, and 
 		<B>you will need to work together to get your ideas across.</B>
 	</P>
 
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/LqOsuibAGNw?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 	</DIV>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		The business of the County Committee meeting is to elect the leadership of the County Party. As a committee member, you will be approached asking to support a candidate. It's the most critical part because it will dictate how the party will behave during the next two-year cycle.
 	</P>
 	
-	<P CLASS="f60 justify">
+	<P CLASS="f40 justify">
 		<B>It's where all drama takes place.</B> You can see what to expect at the first meeting
 		by <A HREF="https://www.revolutions.nyc/expectations/WhatToExpect.pdf">checking out 
 		the flyer distributed</B> at the last Manhattan meeting.
 	</P>
 	
 	<DIV class="videowrapper">
+			<CENTER>
 	 <iframe src="https://www.youtube.com/embed/wPDC_XeTbc4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</CENTER>
 	</DIV>
 	
 	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
-	
-
-</div>
-
-<script>
-
-	var acc = document.getElementsByClassName("accordion2");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-} 
-</script>
 
 
 
