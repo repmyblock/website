4c7cec17f9b536804be93a91fc8716086f132796
Theo Chino <theo@outrageddems.nyc>
2022-03-02 20:41:46 -0500

    Did some cosmetic changes to make the fact that you can continue to log in without having to wait for the email.

--- www/htdocs/exp/register/doneregister.php
+++ www/htdocs/exp/register/doneregister.php
@@ -5,24 +5,25 @@
 
 <div class="row">
 	<div class="main">
-		<P CLASS="f60">
+		<P CLASS="f40">
 			We just sent you another email with the instructions on completing the registration. 
 			<FONT COLOR=BROWN>You won't be able</FONT> to fully continue 
 			using the Rep My Block until you complete the second email verification.
 		</P>
 		
-		<P CLASS="f60">
+		<P CLASS="f40">
 			You will need <FONT COLOR=BROWN><B>to forward that second email</B></FONT> to 
 			<B>notif@repmyblock.org</B> and then you will need <FONT COLOR=BROWN><B>to 
 			click on the verification button</B></FONT>.		
 		</P>
 
-		<P CLASS="f60">
-			While you wait for that second email, you can continue with the login process 
+		<P CLASS="f80">
+			While you wait for that second email, <A HREF="/<?= $middleuri ?>/exp/login/login"><FONT COLOR=BROWN><B>you can 
+				continue with the login</B></FONT></A> process 
 			and setup your personal profile.
 		</P>
 		
-		<p CLASS="f60">
+		<p CLASS="f80">
 			<A HREF="/<?= $middleuri ?>/exp/login/login">Click here to the login page.</A>
 		</P>
 	</DIV>
