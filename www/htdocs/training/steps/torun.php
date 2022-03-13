a5faf1c500d3c64dbd5ca61958197607b3d551fd
Theo Chino <theo@outrageddems.nyc>
2022-03-13 07:10:27 -0400

    Changed the content of the page based on volunteer comments.

--- www/htdocs/training/steps/torun.php
+++ www/htdocs/training/steps/torun.php
@@ -16,7 +16,7 @@
 <div class="main">
 	<DIV CLASS="intro center">
 		<P>
-			<h1 CLASS="intro">I WANT TO RUN FOR COUNTY COMMITTEE</H1>
+			<h1 CLASS="intro">HOW TO RUN FOR COUNTY COMMITTEE</H1>
 		</P>
 		
 		<P CLASS="f40">
@@ -26,19 +26,21 @@
 		</P>
 	</DIV>
 	
-	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
-
-	<H2>The First Step</H2>
+	<H2>Yes, I want to run</H2>
 	
 		<P CLASS="f40 justify">
-			The first step is merely saying, "I am doing it." The process will not make much sense, but everything is like that to dissuade people 
-			from running. We built this website to streamline the process to remove all the hoops from running.
+			The first step is merely saying, I am doing it. The process is designed  
+			to dissuade people from running through a convoluted process. We built this website to 
+			streamline the process to remove as many obstacles as possible that prevent everyday 
+			citizens from running.
 		</P>
 		
 		<P CLASS="f40 justify">
-			The party, like any Non-Profit Organisation or For-Profit Corporation, has bylaws. You can find the bylaws of the various Democratic, 
-			Republican, Conservative and Working Families Parties County Committee at the Board of Election. 
-			<I>(We are working on getting the Bylaws from the other parties, and we will publish them as soon as we have them.)</I>
+			The party, like any Non-Profit Organization or For-Profit Corporation, has bylaws. 
+			You can find the bylaws of the various Democratic, Republican, Conservative and Working 
+			Families Parties County Committee at the Board of Election. 
+			<I>(We are working on getting the Bylaws from the other parties, and we will publish 
+		them as soon as we have them.)</I>
 		</P>
 		
 		<DIV class="videowrapper">
@@ -47,37 +49,42 @@
 			</CENTER>
 		</DIV>
 		
-		<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
+		<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></P>
 	
 
 
 
-	<H2>The Second Step</H2>
+	<H2>Voter Registration Verification</H2>
 	
 
 		<P CLASS="f40 justify">
-			You have decided to run, <B>congratulation!</B>
-			Before you start, we need to ensure that your voter registration with the board of election is correct. 
-			You will require that you are registered in an electoral party based on your convictions.
+			Congratulations, youve  decided to run! Before you start, we need to verify 
+			that your voter registration with the Board of Election is valid and correct. 
+			Through Rep My Block you can verify that you are registered in an electoral 
+			party based on your convictions.
+
 		</P>
 		
 		<P CLASS="f40 justify">
-			If you are not registered to vote or are not registered in a party, you can print a form or go to the DMV website and register. 
-		<B>You can verify your registration automatically on the Rep My Block website.</B>
+			If you are not registered to vote or are not registered in a party, you can 
+			<A HREF="https://vote.nyc/page/register-vote" TARGET=NEW>download and 
+			print a form</A> or go to the 
+			<A HREF="https://dmv.ny.gov/more-info/electronic-voter-registration-application" TARGET="NEW">DMV website</A> 
+			and register. 
 		</P>
 		
 			
-		<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
+		<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></P>
 
 
 
-	<H2>The Third Step</H2>
+	<H2>Petitioning</H2>
 	
 		
 	<P CLASS="f40 justify">
-		This step is <?= $ImportantDates["NY"]["CycleLength"] ?> days long, and in <?= $ImportantDates["NY"]["Cycle"] ?>, 
-		it will start on <?= $ImportantDates["NY"]["LongDate"]["FirstPetitionDay"] ?>, 
-		and it ends on <?= $ImportantDates["NY"]["LongDate"]["LastPetitionDay"] ?>.  
+		This step is <?= $ImportantDates["NY"]["CycleLength"] ?> days long in <?= $ImportantDates["NY"]["Cycle"] ?>, 
+		it will span from <?= $ImportantDates["NY"]["LongDate"]["FirstPetitionDay"] ?>, 
+		to <?= $ImportantDates["NY"]["LongDate"]["LastPetitionDay"] ?>.  
 		Rep My Block will supply a list of registered voters in your district. 
 	</P>
 	
@@ -89,8 +96,13 @@
 	</DIV>
 	
 	<P CLASS="f40 justify">
-		Contrary to the presidential petitioning, won't need to stop random voter in the street but knock on your neighbors' doors. 
-		The Rep My Block website has the walk sheet that is organized by streets. 
+		Contrary to the conventional way of collecting signatures, you won't need to stop 
+		random voters in the street,  but knock on your neighbors' doors. Our  Walk Sheets are organized 
+		by building by building. We eliminate the need to collect three times the amount of signatures in 
+		the hope of collecting enough valid signatures <I>(defined by office and district)</I>. This alone 
+		saves time and energy. Rep My Block petitions have already earned reliability status by 
+		holding up in challenges.
+
 	</P>	
 	
 	<DIV class="videowrapper">
@@ -100,18 +112,20 @@
 		</DIV>
 	
 
-	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></H2></CENTER></P>
+	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></H2></CENTER></P>
 
 
 
-	<H2>The Fourth Step</H2>
+	<H2>Prepare for Filing</H2>
 	
 	<P CLASS="f40 justify">
-		Once you have collected the required numbers of signatures, you will need to bind the petitions according to the New York State Board of Elections' stringent rules.
+		After collecting the required number of signatures, you must bind the petitions according to the 
+		New York State Board of Elections' stringent and detailed rules. 
 	</P>
 
 	<P CLASS="f40 justify">
-		Rep My Block is composed of hundreds of volunteers that have done the process. If you have any questions, a volunteer will be able to answer you put them together.
+		Rep My Block is composed of hundreds of volunteers that have done the process. If you have any 
+		questions, a volunteer will be able to answer you put them together.	
 	</P>
 
 	
@@ -122,18 +136,19 @@
 			</CENTER>
 	</DIV>
 	
-	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
+	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></P>
 
 	
 	
 
 
-	<H2>The Fifth Step</H2>
+	<H2>Filing with the BoE</H2>
 	
 	
 	<P CLASS="f40 justify">
-		This step is where you go to the Board of Election to drop your petitions between 
-		<?= $ImportantDates["NY"]["LongDate"]["FirstSubmitDay"] ?>, and <?= $ImportantDates["NY"]["LongDate"]["LastPetitionDay"] ?>. 
+		Head to the Board of Election and drop off your properly bound petitions between 
+		<?= $ImportantDates["NY"]["LongDate"]["FirstSubmitDay"] ?>, and 
+		<?= $ImportantDates["NY"]["LongDate"]["LastPetitionDay"] ?>. 
 
 	
 	<DIV class="videowrapper">
@@ -143,12 +158,14 @@
 	</DIV>
 	
 	<P CLASS="f40 justify">
-		Rep My Block is a community-based system that uses the power of the community. We will setup dropping points throughout the 
-		community and submit the petition for you. 
-		All you will need to do is to staple your petition together and drop them at a drop off location. 
+		Rep My Block is a community-based system that uses the power of the community. We can set 
+		up dropping points throughout the community and submit the petitions for you. All you will 
+		need to do is to staple your petition together and drop them at a drop-off location. Here 
+		is how to properly file your petition sheets with the Board of Election.
+
 	</P>
 
-	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
+	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></P>
 	
 		
 
@@ -157,16 +174,18 @@
 	
 	
 	<P CLASS="f40 justify">
-		This step is the exciting step where you run for County Committee, and you remind your neighbors to vote for you on <?= $ImportantDates["NY"]["LongDate"]["PrimaryElection"] ?>.
+		This is the exciting step where you run for County Committee! Remind your neighbors to vote for you 
+		on <?= $ImportantDates["NY"]["LongDate"]["PrimaryElection"] ?> <I>(be sure to collect their contact 
+		info during petitioning)</I>.
 	</P>
 	
 	<P CLASS="f40 justify">
-		The night before, we sugest that you put some flyers around your neighborhood to remind your voters to vote for you.
+		The night before, we suggest that you post flyers around your district to remind your voters to vote for you. 	
 	</P>
 	
 	<P CLASS="f40 justify">
-		Rep My Block resources will have a few tools to help you create a quick flyer to put around your block and explain 
-		the Department of Sanitation rules regarding those flyers.
+		Rep My Block resources will have a few tools to help you create a quick flier to post around your block. We 
+		also explain the Department of Sanitation rules about postering.
 	</P>
 
 	
@@ -176,15 +195,15 @@
 			</CENTER>
 	</DIV>
 	
-	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
+	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></P>
 
 
 
-	<H2>The Seventh Step</H2>
-	
+	<H2>Success!</H2>
 	
 	<P CLASS="f40 justify">
-		</B>Finally!</B> You spend 27 hours completing the previous six steps. <B>It is now time to attend your first meeting!</B>
+		</B>Finally!</B> Finally! You spent 27 hours completing the previous six steps and are now a County Committee member. 
+		<B>It is now time to attend your first meeting!</B>
 	</P>
 
 	<DIV class="videowrapper">
@@ -195,8 +214,8 @@
 
 
 	<P CLASS="f40 justify">
-		Rep My Block will have contacted you with other County Committee members that hopefully share your vision, and 
-		<B>you will need to work together to get your ideas across.</B>
+		By now, Rep My Block will have connected you with other County Committee members that hopefully share 
+		your vision, and together you work to get your ideas across.
 	</P>
 
 	<DIV class="videowrapper">
@@ -210,9 +229,14 @@
 	</P>
 	
 	<P CLASS="f40 justify">
-		<B>It's where all drama takes place.</B> You can see what to expect at the first meeting
-		by <A HREF="https://www.revolutions.nyc/expectations/WhatToExpect.pdf">checking out 
-		the flyer distributed</B> at the last Manhattan meeting.
+		The business of the County Committee meeting is to elect the leadership of the County Party. 
+		As a committee member, you will be approached asking to support a candidate. It's the most 
+		critical part because it will dictate how the party will behave during the next two-year cycle.
+	</P>
+	
+	<P CLASS="f40 justify">
+		It's where all drama takes place. You can see what to expect at the first meeting by checking 
+		the videos of those meetings.
 	</P>
 	
 	<DIV class="videowrapper">
@@ -221,7 +245,7 @@
 			</CENTER>
 	</DIV>
 	
-	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register on the Rep My Block website</A></P>
+	<P CLASS="f80 center"><A HREF="/<?= $middleuri ?>/exp/register/register">Register to Run</A></P>
 
 
 
