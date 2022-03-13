1283287406c653d45530487df87e50c39127a36d
Theo Chino <theo@outrageddems.nyc>
2022-03-13 19:24:11 -0400

    Removed the mention of second email.

--- www/htdocs/exp/register/doneregister.php
+++ www/htdocs/exp/register/doneregister.php
@@ -5,27 +5,17 @@
 
 <div class="row">
 	<div class="main">
-		<P CLASS="f40">
-			We just sent you another email with the instructions on completing the registration. 
-			<FONT COLOR=BROWN>You won't be able</FONT> to fully continue 
-			using the Rep My Block until you complete the second email verification.
-		</P>
-		
-		<P CLASS="f40">
-			You will need <FONT COLOR=BROWN><B>to forward that second email</B></FONT> to 
-			<B>notif@repmyblock.org</B> and then you will need <FONT COLOR=BROWN><B>to 
-			click on the verification button</B></FONT>.		
-		</P>
+
 
 		<P CLASS="f80">
-			While you wait for that second email, <A HREF="/<?= $middleuri ?>/exp/login/login"><FONT COLOR=BROWN><B>you can 
-				continue with the login</B></FONT></A> process 
-			and setup your personal profile.
+			To continue, login with the credentials you just created.
 		</P>
 		
 		<p CLASS="f80">
 			<A HREF="/<?= $middleuri ?>/exp/login/login">Click here to the login page.</A>
 		</P>
+	
+		
 	</DIV>
 </DIV>
 
