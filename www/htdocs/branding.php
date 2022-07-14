fbd5c8cd9f1ed43faa811d55a520de21e7b16470
Theo Chino <theo@outrageddems.nyc>
2022-07-14 04:31:37 -0400

    Added the branding information so we don't have to write a long URL

--- www/htdocs/branding.php
+++ www/htdocs/branding.php
@@ -1,4 +1,12 @@
 <?php 
 
  	/* This directory is to keep track of the branding */
+ 	
+ 	if ( empty ($_GET['brand'])) {
+ 		header("Location: /web/exp/index");
+ 		exit();
+ 	} else {
+ 		header("Location: /" . $_GET['brand'] . "/exp/index");
+ 		exit();
+ 	} 	
 ?>
