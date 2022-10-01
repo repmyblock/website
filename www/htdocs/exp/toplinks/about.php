5e2b1fa8d13884125f2c32161f5d2f4d6c816580
Theo Chino <theo@outrageddems.nyc>
2022-10-01 13:50:02 -0400

    Added the Political Tendencies in the list.

--- www/htdocs/exp/toplinks/about.php
+++ www/htdocs/exp/toplinks/about.php
@@ -22,15 +22,48 @@
 		</P>
 		
 		<P CLASS="f40">
-			<B>
 				Rep My Block is a non-partisan effort to collect, organize and make 
 				accessible the full membership of the county committees in New York State. 
 				New York State's county committees are the basis of local government and 
 				are made up of publicly elected representatives. However, county committee 
 				membership information has traditionally been hard to access. 
-			</B>
 		</P>
 	</DIV>
+	
+	<P CLASS="BckGrndElement f80 center">THE MAJOR POLITICAL TENDENCIES</P>
+	
+	<P>
+		The world is run by various political ideologies competing against each other. Rep My Block 
+		is non-partisan; therefore, we welcome all ideologies and help them contact the local 
+		representatives of those ideologies.
+	</P>
+
+	<P>
+    <UL>
+    	<B>Nationalists:</B> <A TARGET="political" HREF="https://www.idgroup.eu">https://www.idgroup.eu</A> - RMB Code: idgroup<BR>
+	    <B>Democrat Union:</B> <A TARGET="political" HREF="https://www.idu.org">https://www.idu.org</A> - RMB Code: idu<BR>
+	    <B>International Alliance of Libertarian Parties:</B> <A TARGET="political" HREF="https://ialp.com">https://ialp.com</A> - RMB Code: libertarians<BR>
+	    <B>Centrist Democrat:</B> <A TARGET="political" HREF="https://www.idc-cdi.com">https://www.idc-cdi.com</A> - RMB Code: idccdi<BR>
+	    <B>Liberals and centrists:</B> <A TARGET="political" HREF="https://liberal-international.org">https://liberal-international.org</A> - RMB Code: liberal<BR>
+	    <B>Progressive Alliance:</B> <A TARGET="political" HREF="https://progressive-alliance.info">https://progressive-alliance.info</A> - RMB Code: progressive<BR>
+	    <B>Social democrats and Socialists:</B> <A TARGET="political" HREF="https://socialistinternational.org">https://socialistinternational.org</A> - RMB Code: socdems<BR>
+	    <B>Greens and regionalists:</B> <A TARGET="political" HREF="https://globalgreens.org">https://globalgreens.org</A> - RMB Code: greens<BR>
+	    <B>Progressive International:</B> <A TARGET="political" HREF="https://progressive.international">https://progressive.international</A> - RMB Code: bernie<BR>
+	    <B>Communists and Workers' parties:</B> <A TARGET="political" HREF="http://www.solidnet.org">http://www.solidnet.org</A> - RMB Code: solidnet<BR>
+	    <B>Socialists Alternative:</B> <A TARGET="political" HREF="https://internationalsocialist.net">https://internationalsocialist.net</A> - RMB Code: socalter<BR>
+	    <B>Pirates:</B> <A TARGET="political" HREF="https://pp-international.net">https://pp-international.net</A> - RMB Code: pirates<BR>
+		</UL>
+	</P>
+	
+	<P>
+		The RMB Code is used once you register to let the leader of these groups contact you so you can effectively 
+		organize with them inside the Republican or Democratic parties of the United States.
+	</P>
+	
+	<P>
+		The goal of Rep My Block is to suppress the role of money in the political conversation while 
+		respecting each individual's ideology without judgment.
+	</P>
 	
 	<P CLASS="BckGrndElement f80 center">ZOOM WITH PAPERBOY LOVE PRINCE</P>
 
