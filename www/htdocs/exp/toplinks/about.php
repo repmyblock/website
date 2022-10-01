1af2bdae58d3c768c985156f86974dc7a98c1f3d
Theo Chino <theo@outrageddems.nyc>
2022-10-01 15:15:23 -0400

    Changed the twitter picture.

--- www/htdocs/exp/toplinks/about.php
+++ www/htdocs/exp/toplinks/about.php
@@ -8,6 +8,9 @@
 	$BigMenu = "home";
 	if ( ! empty ($k)) { $MenuLogin = "logged"; }
 	
+	$HeaderTwitterPicLink = "https://static.repmyblock.org/pics/paste/AboutPageNonPartisan.jpg";
+	$HeaderTwitterDesc = "Get your nominating petition kit here! The County Committee is the most basic committee of the Democratic and Republican Parties; its their backbone. The &hellip; Continue reading Rep My Block &rarr;";
+	
 	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
 	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/funcs/general.php";
 	include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php"; 	
