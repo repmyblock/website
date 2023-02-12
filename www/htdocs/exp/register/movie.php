e84fff4b93d73f72d63f25cc86af96cc543c4f10
Theo Chino <theo@outrageddems.nyc>
2023-02-12 11:57:02 -0500

    Redid the old link of the movie.

--- www/htdocs/exp/register/movie.php
+++ www/htdocs/exp/register/movie.php
@@ -101,6 +101,27 @@
 		on the ground seeking to reform the system. A short documentary by <A HREF="https://www.imdb.com/title/tt20049084/" TARGET="COUNTYMOVIE">Fahim Hamid</A>.
 	</P>
 	
+	<P>
+		<P CLASS="f80">The password is <FONT COLOR=BROWN><B>QCC4ALL!</B></FONT> <I>(all in uppercase)</I></P>
+		
+	</P>
+	
+	
+	<P>		
+		<CENTER>
+			<DIV class="videowrapper">
+				<script src="https://fast.wistia.com/embed/medias/qmt1psxdt4.jsonp" async></script>
+				<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+				<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
+				<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
+				<div class="wistia_embed wistia_async_qmt1psxdt4 videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/qmt1psxdt4/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+			</DIV>		
+		</CENTER>
+	</P>
+</CENTER>
+	
+	<?php /*
+	
 	<P>		
 		<CENTER>
 			<DIV class="videowrapper">
@@ -108,6 +129,8 @@
 			</DIV>		
 		</CENTER>
 	</P>
+	
+	*/ ?>
 	
 	<P class="f40">
 		Please follow the film on <B><A HREF="https://www.facebook.com/countyfilm" TARGET="COUNTYMOVIE">Facebook</A></B> and <B><A HREF="https://www.instagram.com/county_film/">Instagram</A></B>
