b7686ea5e9be4f738038cb69848e4669cb273547
Theo Chino <theo@outrageddems.nyc>
2023-02-12 12:09:43 -0500

    Fixed the wiktia problem

--- www/htdocs/exp/register/movie.php
+++ www/htdocs/exp/register/movie.php
@@ -101,26 +101,38 @@
 		on the ground seeking to reform the system. A short documentary by <A HREF="https://www.imdb.com/title/tt20049084/" TARGET="COUNTYMOVIE">Fahim Hamid</A>.
 	</P>
 	
-	<P>
-		<P CLASS="f80">The password is <FONT COLOR=BROWN><B>QCC4ALL!</B></FONT> <I>(all in uppercase)</I></P>
-		
+	<P class="f60">
+		<CENTER><P CLASS="f40"><B>Press the PLAY button and enter the password <FONT COLOR=BROWN><B>QCC4ALL</B></FONT> </B><I>(all in uppercase)</I> <B>to access the documentary.</B></P></CENTER>
 	</P>
+		
 	
 	
 	<P>		
 		<CENTER>
 			<DIV class="videowrapper">
-				<script src="https://fast.wistia.com/embed/medias/qmt1psxdt4.jsonp" async></script>
-				<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
-				<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
-				<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-				<div class="wistia_embed wistia_async_qmt1psxdt4 videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/qmt1psxdt4/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+				<script src="https://fast.wistia.com/embed/medias/qmt1psxdt4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_embed wistia_async_qmt1psxdt4" style="height:360px;position:relative;width:640px"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/qmt1psxdt4/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div>			
 			</DIV>		
 		</CENTER>
 	</P>
-</CENTER>
+	
+	<P><BR><BR></P>
 	
 	<?php /*
+	<P>		
+		<CENTER>
+			<DIV class="videowrapper">
+				<script src="https://fast.wistia.com/embed/medias/qmt1psxdt4.jsonp" async></script>
+				<script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
+				<div class="wistia_embed wistia_async_qmt1psxdt4 videoFoam=true" style="position:relative;">
+					<div class="wistia_swatch" style="left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;">
+						<img src="https://fast.wistia.com/embed/medias/qmt1psxdt4/swatch" style="filter:blur(5px);object-fit:contain;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
+					</div>
+				</DIV>
+			</DIV>		
+		</CENTER>
+	</P>
+	
+
 	
 	<P>		
 		<CENTER>
