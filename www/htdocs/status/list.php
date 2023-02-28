570e092cfaca51a0143c7e7f26feb988dbc79591
Theo Chino <theo@outrageddems.nyc>
2023-02-28 13:17:28 -0500

    Added the new status screen so people can follow.

---
+++ www/htdocs/status/list.php
@@ -0,0 +1,81 @@
+<?php
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_nolog.php";		
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/db/db_nolog.php";	
+
+	$rmb = new NoLog();
+	$StatusList = $rmb->AreaPetition("1374", "ADED");
+	
+	
+	if ( ! empty ($StatusList) ) {
+		foreach ($StatusList as $var) {
+			if ( ! empty ($var) ) {
+					$TotalCandidate += $var["TOTAL"];
+			}
+		}
+	}
+
+#        require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";
+#        require_once $_SERVER["DOCUMENT_ROOT"] . "/../statlib/Config/Vars.php";
+
+       
+        include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php";
+?>
+<DIV class="main">
+
+        <DIV class="right f80">2023 NYC Petition Status</DIV>
+
+
+		  <P class="f60 p20">
+		          To prepare the petition, we need to verify your Voter Registration.
+		          Please enter the first and last name of the person you want to verify.
+		  </P>
+									
+						<P CLASS="f60">
+									<DIV class="f40">
+									<div id="resp-table">
+										<div id="resp-table-header">
+											<div class="table-header-cell">Party</div>
+											<div class="table-header-cell">District</div>
+											<div class="table-header-cell">Candidates Running</div>
+										</div>
+
+									<div id="resp-table-body">
+											<div class="resp-table-row">
+												<div class="table-body-cell"></div>
+												<div class="table-body-cell"></div>
+												<div class="table-body-cell"><?= $TotalCandidate ?></div>
+										
+											</div>													
+										</div>						
+							
+									
+									<?php 
+			if ( ! empty ($StatusList) ) {
+				foreach ($StatusList as $var) {
+					if ( ! empty ($var) ) {
+			
+										
+		?>
+	
+										<div id="resp-table-body">
+											<div class="resp-table-row">
+												<div class="table-body-cell"><?= $var["CandidateElection_Party"] ?></div>
+												<div class="table-body-cell"><?= $var["CandidateElection_DBTableValue"] ?></div>
+												<div class="table-body-cell"><?= $var["TOTAL"] ?></div>
+								
+											</div>													
+										</div>									
+								<?php }
+							}   
+										
+										} 
+							?>
+
+			</DIV>
+
+	</P>			
+	
+</DIV>
+</DIV>
+
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/common/footer.php"; ?>
---
+++ www/libs/db/db_nolog.php
@@ -0,0 +1,22 @@
+<?php	  	
+require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/db/db_repmyblock.php";  
+
+global $DB;
+class NoLog extends RepMyBlock {
+  
+   function AreaPetition ($ElectionID, $Type) {
+		$sql = "SELECT CandidateElection.ElectionsPosition_ID, CandidateElection_Party, Candidate.CandidateElection_DBTableValue, " . 
+						"COUNT(Candidate.CandidateElection_DBTableValue) AS TOTAL FROM RepMyBlockTwo.Candidate " . 
+						"LEFT JOIN CandidateElection on (Candidate.CandidateElection_ID = CandidateElection.CandidateElection_ID) " . 
+						"WHERE Elections_ID = :ElectionID AND Candidate.CandidateElection_DBTable = :Type AND " . 
+						"Candidate.CandidateElection_DBTableValue != \"XXXXX\" AND Candidate_UniqStateVoterID IS NOT NULL " . 
+						"AND CandidateElection.ElectionsPosition_ID IS NOT NULL " . 
+						"GROUP BY Candidate.CandidateElection_DBTableValue, CandidateElection_Party, ElectionsPosition_ID " .
+						"ORDER BY CandidateElection_Party, CandidateElection_DBTableValue";
+		$sql_vars = array("ElectionID" => $ElectionID, "Type" => $Type);
+	 	return $this->_return_multiple($sql, $sql_vars);
+	}
+	
+}
+?>
+
