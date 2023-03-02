f51203a3ddf5d2075356c87c3e0da37ad2574727
Theo Chino <theo@outrageddems.nyc>
2023-03-01 19:10:50 -0500

    Need to debug the end of the day.

--- www/htdocs/status/list.php
+++ www/htdocs/status/list.php
@@ -36,13 +36,14 @@
 				$hour =  gmdate("H", time());
 				$rounded_min = floor($min/5) * 5;
 				if($rounded_min == 60) {
-				   $rounded_min = 0; $hour++;
+				   $rounded_min = "00"; $hour++;
 				    if ($hour == 24) {
 				    	$hour = "00";
 				    }
 				}
 				
 				include $StatusDirectory . "/" . $hour . "-" . $rounded_min . ".html";
+				echo "\n<!-- Hour: $hour Minute: $rounded_min -->\n";
 			?>
 	
 </DIV>
