3e9d0d4503aed4e721776254fab579e7df92f64a
Theo Chino <theo@outrageddems.nyc>
2023-03-03 17:28:52 -0500

    Moved files around.

--- www/libs/funcs/candidatelogic/NY_CreatePosition.php
+++ www/libs/funcs/candidatelogic/NY_CreatePosition.php
@@ -1,46 +1,78 @@
 <?php
+// Check that this petition for this user doesn't exist.
 
-// This the the logic for populating the candidate field
-if ( ! empty ($CandidateLogic)) {
-	
-	// I need to create the candidate list
-	if ( ! empty ($CandidateLogic["Position"])) {
-		foreach ($CandidateLogic["Position"] as $index => $var) {
-			if ( ! empty ($var)) {				
-				switch ($var) {
-					case "County Committee":
-						$EDAD = sprintf('%02d%03d', $CandidateLogic["AssemblyDistr"], 
-																				$CandidateLogic["ElectDistr"]);
-						$ElectionInfo = $rmb->GetElectionInfo_NewYork_CountyCommittee($CandidateLogic["EnrollPolParty"],
-																																					$CandidateLogic["Gender"], $EDAD);	
-																																																																					
-						// If it's empty we can add the candidate
-						if ( empty ($ElectionInfo[0]["Candidate_ID"])) {
-							$Candidate_ID = $rmb->InsertCandidate_NewYork_CountyCommittee(
-														$SystemUser_ID, $CandidateLogic["Voter_ID"], $DatedFiles,
-														$DatedFilesID, $ElectionInfo[0]["CandidateElection_ID"], $EDAD, 
-														$CandidateLogic["EnrollPolParty"], $CandidateLogic["PetitionName"],
-														$CandidateLogic["PetitionAddress"], $CandidateLogic["CountyCode"]);
-														
-							// Now we need to add the voter list for this person
-							$InfoArray["Election"] = "CC";
-							$InfoArray["ElectDistr"] = $CandidateLogic["ElectDistr"];
-					    $InfoArray["AssemblyDistr"] = $CandidateLogic["AssemblyDistr"];
-							$InfoArray["Party"] = $CandidateLogic["EnrollPolParty"];    
-							$Counter = $rmb->PrepDisctictVoterRoll($Candidate_ID, $DatedFiles, $DatedFilesID, $InfoArray);
-							$rmb->UpdateCandidateCounterForVoter($Candidate_ID, $Counter);
-							if ( $ElectionInfo[0]["Elections_ID"] > 0 ) {
-								$rmb->UpdateElectionCounterForVoter($ElectionInfo[0]["Elections_ID"], $Counter);
-							}
-						}
-						break;
-						
-					default:
-						break;
-				}
-			}
-		}
+WriteStderr($PetitionData, "PetitionData");	
+
+$ElectionType = $rmb->FindElectionType(	$PetitionData["Elections_ID"], $PetitionData["Voters_RegParty"],
+																				$PetitionData["TypeElection"], 
+																				$PetitionData["TypeValue"]);																	
+if ( empty ($ElectionType)) {
+
+	$PartyCall = $rmb->FindInPartyCall(	$PetitionData["Elections_ID"], $PetitionData["County_ID"], 
+																			$PetitionData["Voters_RegParty"], $PetitionData["TypeElection"], 
+																			$PetitionData["TypeValue"]);
+																			
+	if ( empty ($PartyCall)) {
+		$CandidateElection_Text = "Please contact because the district doesn't exist in the Party Call";
 	}
+
+	$CountyName = $rmb->DB_WorkCounty($PetitionData["County_ID"]);			
+	preg_match('/(\d{2})(\d{3})/', $PetitionData["TypeValue"], $District, PREG_OFFSET_CAPTURE);
+	
+	switch($PartyCall["CandidatePositions_ID"]) {
+		case '1':
+			$CandidateElection_Text = $CountyName . " County Committee";
+			$PositionType = "party";
+			$NumberCandidates = $PartyCall["ElectionsPartyCall_NumberUnixSex"];
+			$CandidateElection_PetitionText = "Member of the " . $CountyName . " Democratic County Committee from the " .
+																				ordinal(ltrim($District[2][0], "0")) . " election district in the " . 
+																				ordinal(ltrim($District[1][0], "0")) . " assembly district";
+		break;
+
+		case '13':	
+			$CandidateElection_Text = $CountyName . " County Committee";
+			$PositionType = "party";
+			$NumberCandidates = $PartyCall["ElectionsPartyCall_NumberUnixSex"];
+			$CandidateElection_PetitionText = "Member of the " . $CountyName . " Republican County Committee from the " .
+																				ordinal(ltrim($District[2][0], "0")) . " election district in the " . 
+																				ordinal(ltrim($District[1][0], "0")) . " assembly district";
+		break;
+	}
+
+	$MatchTableName = array(
+			"Elections_ID" => $PetitionData["Elections_ID"], 
+			"ElectionsPosition_ID" => $PartyCall["CandidatePositions_ID"], 
+			"CandidateElection_PositionType" => $PartyCall["ElectionsPartyCall_PositionType"], 
+			"CandidateElection_Party" => $PetitionData["Voters_RegParty"], 
+			"CandidateElection_Text" => $CandidateElection_Text, 
+			"CandidateElection_PetitionText" => $CandidateElection_PetitionText, 
+			"CandidateElection_URLExplain" => NULL, 
+			"CandidateElection_Number" => $NumberCandidates, 
+			"CandidateElection_DisplayOrder" => "100", 
+			"CandidateElection_Display" => NULL, 
+			"CandidateElection_Sex" => NULL, 
+			"CandidateElection_DBTable" => $PetitionData["TypeElection"], 
+			"CandidateElection_DBTableValue" => $PetitionData["TypeValue"],
+	);
+				
+	$ElectionType = $rmb->CreatePositionEntry($MatchTableName);
+	
+} else {
+	
+	$ElectionType = $ElectionType[0]["CandidateElection_ID"];
 }
+
+$Candidate = $rmb->InsertCandidate($PetitionData["SystemUser_ID"], $PetitionData["UniqNYSVoterID"], 
+											$PetitionData["Voters_ID"], 
+											$PetitionData["County_ID"], $ElectionType, 
+											$PetitionData["CPrep_Party"], $PetitionData["FullName"],
+											$PetitionData["AddressLine1"] . "\n" . $PetitionData["AddressLine2"], 
+											$PetitionData["TypeElection"], $PetitionData["TypeValue"], NULL, 
+											"published", $PetitionData["ActiveTeam_ID"]);
+
+$CandidateSet = $rmb->NextPetitionSet($PetitionData["SystemUser_ID"]);
+$FinalCandidate = $rmb->InsertCandidateSet($Candidate["Candidate_ID"], $CandidateSet["CandidateSet"], 
+																						$PetitionData["CPrep_Party"], $PetitionData["County_ID"]);		
+
 
 ?>
