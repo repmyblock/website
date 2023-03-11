08951ad6cb131fa51326112898e8e079d718e61a
Theo Chino <theo@outrageddems.nyc>
2023-03-11 12:03:13 -0500

    Added a menu to access the walksheets

---
+++ www/htdocs/lgd/walksheets/addwalksheets.php
@@ -0,0 +1,23 @@
+<?php
+	if ( ! empty ($k)) { $MenuLogin = "logged";  }  
+	$Menu = "team";
+
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";	
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/db/db_teams.php";
+	
+  if (empty ($URIEncryptedString["SystemUser_ID"])) { goto_signoff(); }
+  
+  // Passtru function to add the Team Priviledge to the user.  
+  $rmb = new Teams();
+	$rmb->UpdateAutoTeam(PERM_MENU_WALKSHEET, $URIEncryptedString["SystemUser_ID"]); 
+			
+
+	header("Location: /" .  CreateEncoded ( array( 
+								"SystemUser_ID" => $URIEncryptedString["SystemUser_ID"],
+						    "FirstName" => $URIEncryptedString["FirstName"],
+						    "LastName" => $URIEncryptedString["LastName"],
+						    "UniqNYSVoterID" => $URIEncryptedString["UniqNYSVoterID"],
+						   	"SystemAdmin" => $URIEncryptedString["SystemAdmin"]
+					)) . "/lgd/walksheets/petitions");
+	exit();
+?>
