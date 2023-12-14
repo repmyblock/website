a2b008e780a510b8e90bd6f30a815d5af442ee54
Theo Chino <theo@outrageddems.nyc>
2023-12-14 17:24:46 -0500

    New Files.

---
+++ www/htdocs/brand/showthebooks/brandheader.php
@@ -0,0 +1,19 @@
+<?php
+	$BrandingName = "showthebooks";
+	$BrandingTitle = "Run for any type of office, any party";
+	$BrandingMaintainer = "This page is maintained by the <B><A HREF=" .
+												"\"https://www.repmyblock.org\" TARGET=\"RMB\">Rep My Block group to showcase branding</A></B>. " . 
+												"Check their website at <B><A HREF=\"https://www.repmyblock.org\" TARGET=\"RMB\">https://www.repmyblock.org</A>.";
+												
+	$HeaderTwitter = "yes";
+	$HeaderTwitterTitle = "Rep My Block - Rep My Block";
+	$HeaderTwitterPicLink = "https://static.repmyblock.org/pics/paste/GenericMyBlock.jpg";
+	$HeaderTwitterDesc = "Run For any type of office.";
+	$HeaderOGTitle = "Run For any type of office.";
+	$HeaderOGDescription = "Run For any type of office.";
+	$HeaderOGImage = "https://static.repmyblock.org/pics/paste/GenericMyBlock.jpg"; 
+	$HeaderOGImageWidth = "921";
+	$HeaderOGImageHeight = "575";
+	
+	$imgtoshow = "/brand/generic/GenericRepMyBlock.png";
+?>
---
+++ www/htdocs/brand/showthebooks/saveinformation.php
@@ -0,0 +1,57 @@
+<?php
+	include "brandheader.php";
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../statlib/Config/Vars.php";
+
+	if ( ! empty ($_POST["CheckRegistration"])) {
+		
+		require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/funcs/general.php";
+
+		header("Location: /" . CreateEncoded (
+														array("FirstName" => trim($_POST["FirstName"]),	
+																	"LastName" => trim($_POST["LastName"]))) .
+						"/brand/" . $BrandingName . "/findvoter");
+		exit();
+	}
+	
+	if ( $URIEncryptedString["SystemQuerySaveID"] > 0 ) {
+		$EmailToEmail = $BrandingName . "+" . $URIEncryptedString["SystemQuerySaveID"] . "@register.repmyblock.org";
+	} else {
+		$EmailToEmail = $BrandingName . "@register.repmyblock.org";
+	}
+	
+	include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php"; 
+?>
+<DIV class="main">
+		
+	<DIV class="right f80"><?= $BrandingTitle ?></DIV>
+	
+			
+		<P class="f60 p20">
+			Thanks for filling out the registration form. Someone for Save Section 9 will contact 
+			you shortly to help get the petition.
+		</P>
+		
+		<P class="f60">
+			You can create a username by sending an email to 
+			<A HREF="mailto:<?= $EmailToEmail ?>?subject=I want to register&body=DO NOT CHANGE THE SUBJECT. Just send the email as is for the computer to reply with the link." TARGET="emailreg"><B><?=$EmailToEmail ?></B></A>.
+		</P>
+		
+			
+			<P class="f50">
+				<?= $BrandingMaintainer ?>
+			</P>
+			
+		<P class="f40">
+			By clicking the "Register" button, you are creating a 
+			RepMyBlock account, and you agree to RepMyBlock's 
+			<A HREF="/text/terms">Terms of Use</A> and 
+			<A HREF="/text/privacy">Privacy Policy.</A>
+		</P>
+
+
+
+	</FORM>
+</DIV>
+		
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/common/footer.php"; ?>
