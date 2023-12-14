7bb4209b56ce11b000a133b3f972ace8847b3ba8
Theo Chino <theo@outrageddems.nyc>
2023-12-14 02:39:23 -0500

    Fixed the QCC4ALL again.

---
+++ www/htdocs/brand/qcc4all/brandheader.php
@@ -0,0 +1,19 @@
+<?php
+	$BrandingName = "qcc4all";
+	$BrandingTitle = "Run for any type of office, any party";
+	$BrandingMaintainer = "This page is maintained by the <B><A HREF=" .
+												"\"https://www.QCC4All.org\" TARGET=\"QCC4ALL\">Queens County Committee for All</A></B>. " . 
+												"Check their website at <B><A HREF=\"https://www.QCC4All.org\" TARGET=\"QCC4ALL\">https://www.QCC4All.org</A>.";
+												
+	$HeaderTwitter = "yes";
+	$HeaderTwitterTitle = "Rep My Block - Queens County Committee for All";
+	$HeaderTwitterPicLink = "https://static.repmyblock.org/pics/paste/QCC4ALL.jpg";
+	$HeaderTwitterDesc = "Run For any type of office.";
+	$HeaderOGTitle = "Run For any type of office.";
+	$HeaderOGDescription = "Run For any type of office.";
+	$HeaderOGImage = "https://static.repmyblock.org/pics/paste/QCC4ALL.jpg"; 
+	$HeaderOGImageWidth = "610";
+	$HeaderOGImageHeight = "415";
+	
+	$imgtoshow = "/brand/qcc4all/qcc4all.png";
+?>
---
+++ www/htdocs/brand/qcc4all/index.php
@@ -0,0 +1,42 @@
+<?php
+	include "brandheader.php";
+	require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/common/verif_sec.php";
+	
+	include $_SERVER["DOCUMENT_ROOT"] . "/common/headers.php"; 
+	if ( $MobileDisplay == true ) { $TypeEmail = "email"; $TypeUsername = "username";
+	} else { $TypeEmail = "text"; $TypeUsername = "text"; }
+?>
+<DIV class="main">
+	<DIV class="right f80"><?= $BrandingTitle ?></DIV>
+	
+			<P class="f50">
+				We are members of the Queens Democratic County Committee working to make this elected body more 
+				democratic, transparent, inclusive, accountable, and accessible.
+			</P>
+			
+			<P class="f80bold center">
+				<A HREF="/<?= $middleuri ?>/brand/<?= $BrandingName ?>/search">Click here to check your registration</A>
+			</P>
+		
+			<P class="f60">
+				<B>Please note:</B> We're not experts. QCC4All members are just people like you who
+				decided to run and pieced together the rules--which can change at any minute. But
+				this process is supposed to be open to grassroots voters and that's why we all need to
+				participate--to grow democracy and have our voices heard in the Party, which makes
+				so many crucial decisions in Queens.
+			</P>
+			
+			<P>
+				<A HREF="/brand/<?= $BrandingName ?>/HowToRunForCountyCommittee.pdf">Download the informal guide</A> <I>(created Jan 3rd 2020)</I>
+			</P>
+			
+			<P class="f50">
+				<?= $BrandingMaintainer ?>
+			</P>
+			
+	</FORM>
+</DIV>
+		
+<?php include $_SERVER["DOCUMENT_ROOT"] . "/common/footer.php"; ?>
+
+
