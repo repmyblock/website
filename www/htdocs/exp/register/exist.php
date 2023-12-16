33d24b4a42238ef419805e4e8a058b6663a45c40
Theo Chino <theo@outrageddems.nyc>
2023-12-16 03:43:55 -0500

    Cosmetic change.

--- www/htdocs/exp/register/exist.php
+++ www/htdocs/exp/register/exist.php
@@ -12,8 +12,8 @@
 		</P>	
 
 		<P class="f60">		
-			Click here if <A HREF="/<?= $k ?>/exp/forgot/forgotuser">you forgot your username</A> or 
-			here if <A HREF="/web/exp/forgot/forgotpwd">you forgot your password</A>.
+			Click here if you <A HREF="/<?= $k ?>/exp/forgot/forgotuser">forgot your username</A> or 
+			here if you <A HREF="/web/exp/forgot/forgotpwd">forgot your password</A>.
 		</P>
 				
 		<p class="f80">
