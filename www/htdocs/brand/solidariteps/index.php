b611ace4086e54b2240287e233dea3a1054882b2
Theo Chino <theo@outrageddems.nyc>
2023-12-15 19:26:48 -0500

    Made a few more changes.

--- www/htdocs/brand/solidariteps/index.php
+++ www/htdocs/brand/solidariteps/index.php
@@ -31,21 +31,27 @@
 				<B>Retrouvez le reportage ici :</B>
 			</P>
 			
-			<P class="f60">
-				
-			<P class="f80"></B><A HREF="https://www.tf1.fr/tmc/martin-weill/videos/les-reportages-de-martin-weill-new-york-la-fin-du-reve-partie-1-94816573.html">Partie 1 : la d&eacute;mesure</A></B></P>
+			<UL class="f60">
+				<P class="f60">
+					Elle vient d'&eacute;tre class&eacute;e la ville la plus ch&egrave;re du monde. 
+				</P>
+							
+			<P class="f80"></B><A  TARGET="TMC" HREF="https://www.tf1.fr/tmc/martin-weill/videos/les-reportages-de-martin-weill-new-york-la-fin-du-reve-partie-1-94816573.html">Partie 1 : la d&eacute;mesure</A></B></P>
 				<UL class="f60">
-					Elle vient d'&eacute;tre class&eacute;e la ville la plus ch&egrave;re du monde. A Manhattan, des 
+				A Manhattan, des 
 				penthouses se vendent &agrave; 200 millions de dollars et les agents immobiliers 
 				sp&eacute;cialis&eacute;s dans le luxe sont devenus 
-				les nouveaux rois de la cit&eacute;. New York, terre d'accueil historique, serait-elle en
+				les nouveaux rois de la cit&eacute;. 
+			</UL>
+				
+				<P class="f60">
+				New York, terre d'accueil historique, serait-elle en
 				train de devenir un 
 				repaire pour millionnaires ? Qui peut encore croquer dans la Grosse Pomme ? 
-			</UL>
+				
 			</P>
 			
-			<P class="f60">
-			<P class="f80"><B><A HREF="https://www.tf1.fr/tmc/martin-weill/videos/les-reportages-de-martin-weill-new-york-la-fin-du-reve-partie-2-81510722.html">Partie 2 : les in&eacute;galit&eacute;s</A></B></P>
+			<P class="f80"><B><A TARGET="TMC" HREF="https://www.tf1.fr/tmc/martin-weill/videos/les-reportages-de-martin-weill-new-york-la-fin-du-reve-partie-2-81510722.html">Partie 2 : les in&eacute;galit&eacute;s</A></B></P>
 				<UL class="f60">
 				De Central Park au 
 				Bronx menac&eacute; par la gentrification, des th&eacute;&acirc;tres de Broadway aux refuges 
@@ -53,13 +59,19 @@
 				craquer, Martin Weill a rencontr&eacute; ceux qui courent apr&eacute;s le r&ecirc;ve new-yorkais... 
 				et ceux qui 
 				n'y croient plus. 
-				Bienvenue dans la ville la plus attractive du globe mais aussi l'une des plus in&eacute;galitaires.
 				</UL>
+		
+		
+				<P class="f60">
+				Bienvenue dans la ville la plus attractive du globe mais aussi l'une des plus in&eacute;galitaires.
 			</P>
+			
+					</UL>
+		
 			
 			
 				<P class="f80bold center">
-				<A HREF="/<?= $middleuri ?>/brand/<?= $BrandingName ?>/aide">Regardez le reportage et ensuite clickez ici pour nous aider.</A>
+				<A HREF="/<?= $middleuri ?>/brand/<?= $BrandingName ?>/aide">Appuyez ici pour nous aider.</A>
 			</P>
 			
 			
