5fbdf2b466e9b61761687b4b7c61218e8396409e
Theo Chino <theo@outrageddems.nyc>
2023-12-24 19:42:09 -0500

    Changed the RMB internal code from the About Page. Will be sending to branded pages later.

--- www/htdocs/exp/toplinks/about.php
+++ www/htdocs/exp/toplinks/about.php
@@ -44,19 +44,19 @@
 	</P>
 
   <UL class="f40 adpad">
-  	<B><A TARGET="political" TARGET="political" HREF="https://www.idgroup.eu">Nationalists</A>:</B> Conservative Party USA: <A TARGET="political" HREF="https://conservativepartyusa.org">https://conservativepartyusa.org</A> - RMB Code: idgroup<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://www.idu.org">Democrat Union</A>:</B> Republican National Committee: <A TARGET="political" HREF="https://gop.com">https://gop.com</A> - RMB Code: intdu<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://ialp.com">International Alliance of Libertarian Parties</A>:</B> Libertarian: <A TARGET="political" HREF="https://www.lp.org">https://www.lp.org/</A> - RMB Code: libertarians<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://www.idc-cdi.com">Centrist Democrat</A>:</B> Frederick Douglass Foundation: <A TARGET="political" HREF="https://fdfnational.org">https://fdfnational.org</A> - RMB Code: idccdi<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://liberal-international.org">Liberals and centrists</A>:</B> Center for New Liberalism: <A TARGET="political" HREF="https://cnliberalism.org">https://cnliberalism.org</A> - RMB Code: liberal<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://progressive-alliance.info">Progressive Alliance</A>:</B> Progressive Democrats of America: <A TARGET="political" HREF="https://pdamerica.org">https://pdamerica.org</A> - RMB Code: progressive<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://socialistinternational.org">Social democrats and Socialists</A>:</B> Social Democrats of America: <A TARGET="political" HREF="https://socialists.us">https://socialists.us</A> - RMB Code: socdems<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://globalgreens.org">Greens and regionalists</A>:</B> Green Party US: <A TARGET="political" HREF="https://www.gp.org">https://www.gp.org</A> - RMB Code: greens<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://progressive.international">Progressive International</A>:</B> Democrat Socialists of America: <A TARGET="political" HREF="https://www.dsausa.org">https://www.dsausa.org/</A> - RMB Code: bernie<BR>
-    <B><A TARGET="political" TARGET="political" HREF="http://www.solidnet.org">Communists and Workers' parties</A>:</B> Communist Party USA: <A TARGET="political" HREF="https://www.cpusa.org">https://www.cpusa.org</A> - RMB Code: solidnet<BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://internationalsocialist.net">Socialists Alternative</A>:</B> Socialist Alternative: <A TARGET="political" HREF="https://socialistalternative.org">https://socialistalternative.org</A> - RMB Code: socalter<BR>
-		<B><A TARGET="political" TARGET="political" HREF="https://ipa-aip.org">International People's Assembly</A>:</B> Party for Socialism and Liberation: <A TARGET="politital" HREF="https://pslweb.org">https://pslweb.org</A> - RMB Code: ipassembly</A><BR>
-    <B><A TARGET="political" TARGET="political" HREF="https://pp-international.net">Pirates Parties International</A>:</B> United States Pirate Party: <A TARGET="political" HREF="https://uspirates.org">https://uspirates.org</A> - RMB Code: pirates<BR>
+  	<B><A TARGET="political" TARGET="political" HREF="https://www.idgroup.eu">Nationalists</A>:</B> Conservative Party USA: <A TARGET="political" HREF="https://conservativepartyusa.org">https://conservativepartyusa.org</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://www.idu.org">Democrat Union</A>:</B> Republican National Committee: <A TARGET="political" HREF="https://gop.com">https://gop.com</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://ialp.com">International Alliance of Libertarian Parties</A>:</B> Libertarian: <A TARGET="political" HREF="https://www.lp.org">https://www.lp.org/</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://www.idc-cdi.com">Centrist Democrat</A>:</B> Frederick Douglass Foundation: <A TARGET="political" HREF="https://fdfnational.org">https://fdfnational.org</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://liberal-international.org">Liberals and centrists</A>:</B> Center for New Liberalism: <A TARGET="political" HREF="https://cnliberalism.org">https://cnliberalism.org</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://progressive-alliance.info">Progressive Alliance</A>:</B> Progressive Democrats of America: <A TARGET="political" HREF="https://pdamerica.org">https://pdamerica.org</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://socialistinternational.org">Social democrats and Socialists</A>:</B> Social Democrats of America: <A TARGET="political" HREF="https://socialists.us">https://socialists.us</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://globalgreens.org">Greens and regionalists</A>:</B> Green Party US: <A TARGET="political" HREF="https://www.gp.org">https://www.gp.org</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://progressive.international">Progressive International</A>:</B> Democrat Socialists of America: <A TARGET="political" HREF="https://www.dsausa.org">https://www.dsausa.org/</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="http://www.solidnet.org">Communists and Workers' parties</A>:</B> Communist Party USA: <A TARGET="political" HREF="https://www.cpusa.org">https://www.cpusa.org</A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://internationalsocialist.net">Socialists Alternative</A>:</B> Socialist Alternative: <A TARGET="political" HREF="https://socialistalternative.org">https://socialistalternative.org</A><BR>
+		<B><A TARGET="political" TARGET="political" HREF="https://ipa-aip.org">International People's Assembly</A>:</B> Party for Socialism and Liberation: <A TARGET="politital" HREF="https://pslweb.org">https://pslweb.org</A></A><BR>
+    <B><A TARGET="political" TARGET="political" HREF="https://pp-international.net">Pirates Parties International</A>:</B> United States Pirate Party: <A TARGET="political" HREF="https://uspirates.org">https://uspirates.org</A><BR>
 	</UL>
 	
 	<P class="f40 adpad">
