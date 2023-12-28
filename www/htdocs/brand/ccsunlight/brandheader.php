d3d4e0ec70f7556b99efdb30b2bb902eed5eb304
Theo Chino <theo@outrageddems.nyc>
2023-12-28 05:00:24 -0500

    Creating a CCSUNLIGHT Branding version to showcase to Jon.

---
+++ www/htdocs/brand/ccsunlight/brandheader.php
@@ -0,0 +1,22 @@
+<?php
+	$BrandingName = "ccsunlight";
+	$BrandingTitle = "We are a group of citizens shedding light on local political party membership in New York, why they matter, and how to get involved.";
+	$BrandLink = "https://www.ccsunlight.org";
+	$BrandTargetName = "CCS";
+	$BrandingMaintainer = "This page is maintained by the <B><A HREF=\"" . $BrandLink . "\" TARGET=\"" . 
+												$BrandTargetName . "\">County Committee Sunlight Project</A></B>. " . 
+												"Check their website at <B><A HREF=\"" . $BrandLink . "\" TARGET=\"" . 
+												$BrandTargetName ."\">" . $BrandLink ."</A>.";
+	
+	$HeaderTwitter = "yes";
+	$HeaderTwitterTitle = "Rep My Block - Rep My Block";
+	$HeaderTwitterPicLink = "https://static.repmyblock.org/pics/paste/GenericMyBlock.jpg";		
+	$HeaderTwitterDesc = $BrandingTitle;
+	$HeaderOGTitle = $BrandingTitle;
+	$HeaderOGDescription = $BrandingTitle;
+	$HeaderOGImage = $HeaderTwitterPicLink;
+	$HeaderOGImageWidth = "921";
+	$HeaderOGImageHeight = "575";
+	
+	$imgtoshow = "/brand/" . $BrandingName . "/ccsunlight.png";
+?>
