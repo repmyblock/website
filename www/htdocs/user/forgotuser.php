3ee07d29221a0bc2a11ef68851fc0740e23b8869
Theo Chino <theo@outrageddems.nyc>
2024-04-22 23:57:12 -0400

    Added a check to make sure that it doesn't send useless emails.

--- www/htdocs/user/forgotuser.php
+++ www/htdocs/user/forgotuser.php
@@ -6,14 +6,13 @@
 	$r = new login();
 
 	if ( ! empty ($_POST["signin"])) {
-		
 		$hashtable = hash(md5, PrintRandomText(40));
 		$r->UpdateHash($_POST["email"], $hashtable);
-		$result = $r->CheckEmail($_POST["email"]);
-		
-		SendForgotUsername($_POST["email"],  $hashtable);
+		$result = $r->CheckEmail($_POST["email"]);		
+		if ( ! empty ($result)) {
+			SendForgotUsername($_POST["email"],  $hashtable);
+		}
 		header("Location: sentusername");
-		
 		exit();
 	}
 	
