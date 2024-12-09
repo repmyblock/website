505de885430aff06ac4feb8cb827a6d7b51a32e8
Theo Chino <theo@outrageddems.nyc>
2024-12-09 09:00:37 -0500

    Moved the movie to the howto screen.

--- www/htdocs/training/steps/torun.php
+++ www/htdocs/training/steps/torun.php
@@ -61,14 +61,30 @@
 		
 
 		</P>
-		
+	
+	<P>		
+		<CENTER>
+			<DIV class="videowrapper">
+				<script src="https://fast.wistia.com/embed/medias/qmt1psxdt4.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_embed wistia_async_qmt1psxdt4" style="height:360px;position:relative;width:640px"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/qmt1psxdt4/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div>			
+			</DIV>		
+		</CENTER>
+	</P>
+	
+	<P>
+		&nbsp;
+	</P>
+			
+	<P class="f80 center"><A HREF="/<?= $middleuri ?>/register/user">Register to Run</A></P>
+	
+
+<?php /*		
 	<P>
 		<CENTER>
 			<A class="action-runfor" HREF="/<?= $middleuri ?>/register/movie"><IMG SRC="/images/documentary/CountyDocumentary.png"></A>
 		</CENTER>
 	</P>
+	*/ ?>
 	
-	<P class="f80 center"><A HREF="/<?= $middleuri ?>/register/user">Register to Run</A></P>
 	
 	<P class="BckGrndElement f80 center">Yes, I want to run</P>
 	<h2>Yes, I want to run</h2>
