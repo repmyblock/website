be2c153fbbe50d546526bb8562adbe0e03aed154
Theo Chino <theo@outrageddems.nyc>
2025-02-14 13:58:31 -0500

    Changed the vendor data to the database. Todo: Move it to a file on the server.

---
+++ www/libs/db/db_display.php
@@ -0,0 +1,19 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/../libs/mysql/queries.php";
+global $DB;
+
+class display extends queries {
+
+  function display ($debug = 0, $DBFile = "DB_OutragedDems") {
+	  require $_SERVER["DOCUMENT_ROOT"] . "/../statlib/DBsLogins/" . $DBFile . ".php";
+	  $DebugInfo["DBErrorsFilename"] = $DBErrorsFilename;
+	  $DebugInfo["Flag"] = $debug;
+	 	$this->queries($databasename, $databaseserver, $databaseport, $databaseuser, $databasepassword, $sslkeys, $DebugInfo);
+  }
+  
+  function listvendors() {
+		return $this->_return_multiple("SELECT * FROM Vendor ORDER BY Vendor_Order");
+	}
+	
+}
+?>
