b68e248f032c6dc9a3f2e8c71a44b23a0360f0ac
Theo Chino <theo@outrageddems.nyc>
2025-02-19 15:37:07 -0500

    Fixed the Tik Tok menu

--- www/htdocs/voter/detail.php
+++ www/htdocs/voter/detail.php
@@ -100,7 +100,7 @@
 				      <?php if (! empty ($var["CandidateProfile_Twitter"])) { ?>Twitter: <A TARGET="NEW" HREF="https://twitter.com/<?= $var["CandidateProfile_Twitter"] ?>">@<?= $var["CandidateProfile_Twitter"] ?></A><?php } ?> 
 				     	<?php if (! empty ($var["CandidateProfile_Facebook"])) { ?>Facebook: <A TARGET="NEW" HREF="https://facebook.com/<?= $facebook_url ?>"><?= $facebook_url ?></A><?php } ?> 
 				      <?php if (! empty ($var["CandidateProfile_Instagram"])) { ?>Instagram: <A TARGET="NEW" HREF="https://instagram.com/<?= $var["CandidateProfile_Instagram"] ?>">@<?= $var["CandidateProfile_Instagram"] ?></A><?php } ?> 
-				      <?php if (! empty ($var["CandidateProfile_TikTok"])) { print $var["CandidateProfile_TikTok"] . " - "; } ?> 
+				      <?php if (! empty ($var["CandidateProfile_TikTok"])) { ?>Tik Tok: <A TARGET="NEW" HREF="https://www.tiktok.com/<?= $var["CandidateProfile_TikTok"] ?>"><?= $var["CandidateProfile_TikTok"] ?></A><?php } ?> 
 				      <?php if (! empty ($var["CandidateProfile_YouTube"])) { ?>YouTube: <A TARGET="YouTubeRMB" HREF="https://youtube.com/<?= $var["CandidateProfile_YouTube"] ?>"><?= $var["CandidateProfile_YouTube"] ?></A><?php } ?> 
 				      <?php if (! empty ($var["CandidateProfile_FaxNumber"])) { print $var["CandidateProfile_FaxNumber"]; }  ?>
 			      </P>
