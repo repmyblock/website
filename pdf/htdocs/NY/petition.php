ae9c8a8db960bc990d3afcee0897e81b67a41503
Theo Chino <theo@outrageddems.nyc>
2025-02-23 12:33:45 -0500

    Just removed the auto city write.

--- pdf/htdocs/NY/petition.php
+++ pdf/htdocs/NY/petition.php
@@ -276,35 +276,35 @@ if ( ! empty ($URIEncryptedString["CustomData"] )) {
 switch ($pdf_NY_petition->county) {
 	case "Bronx":
 		#$pdf_NY_petition->AutoFillCounty = "BX";
-		$pdf_NY_petition->AutoFillCity = "Bronx, NY";
+		$pdf_NY_petition->AutoFillCity = ", NY";
 		$pdf_NY_petition->TypeOfTown = "County";
 		$pdf_NY_petition->ShowApt = true;
 		break;
 		
 	case "New York":
 		#$pdf_NY_petition->AutoFillCounty = "NY";
-		$pdf_NY_petition->AutoFillCity = "New York, NY";
+		$pdf_NY_petition->AutoFillCity = ", NY";
 		$pdf_NY_petition->TypeOfTown = "County";
 		$pdf_NY_petition->ShowApt = true;
 		break;
 		
 	case "Richmond":
 		#$pdf_NY_petition->AutoFillCounty = "RH";
-		$pdf_NY_petition->AutoFillCity = "Staten Island, NY";
+		$pdf_NY_petition->AutoFillCity = ", NY";
 		$pdf_NY_petition->TypeOfTown = "County";
 		$pdf_NY_petition->ShowApt = true;
 		break;
 		
 	case "Queens":
 		#$pdf_NY_petition->AutoFillCounty = "QN";
-		$pdf_NY_petition->AutoFillCity = "Queens, NY";
+		$pdf_NY_petition->AutoFillCity = ", NY";
 		$pdf_NY_petition->TypeOfTown = "County";
 		$pdf_NY_petition->ShowApt = true;
 		break;
 
 	case "Kings":
 		#$pdf_NY_petition->AutoFillCounty = "KG";
-		$pdf_NY_petition->AutoFillCity = "New York, NY";
+		$pdf_NY_petition->AutoFillCity = ", NY";
 		$pdf_NY_petition->TypeOfTown = "County";
 		$pdf_NY_petition->ShowApt = true;
 		break;
