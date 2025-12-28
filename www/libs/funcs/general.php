abca6e34bf7154f4794939067d58225145bc0088
Theo Chino <theo@outrageddems.nyc>
2025-12-28 03:56:55 -0500

    Added the function for the Rep My Block ambassador.

--- www/libs/funcs/general.php
+++ www/libs/funcs/general.php
@@ -18,6 +18,7 @@ const PERM_OPTION_ALLPOS = 16384;
 const PERM_MENU_OBJECTIONS = 32768;
 const PERM_MENU_VENDORS = 65536;
 const PERM_MENU_PRESS = 131072;
+const PERM_MENU_AMBASSADOR = 262144;
 
 function ordinal($number) {
   $ends = array('th','st','nd','rd','th','th','th','th','th','th');
