097a846f7953a3fcb412817c29af8074dec0cb6f
Theo Chino <theo@outrageddems.nyc>
2026-02-09 14:00:10 -0500

    Fixed a lot of small issues.

--- www/libs/db/db_display.php
+++ www/libs/db/db_display.php
@@ -4,13 +4,14 @@ global $DB;
 
 class display extends queries {
 
-  function display ($debug = 0, $DBFile = "DB_OutragedDems") {
-	  require $_SERVER["DOCUMENT_ROOT"] . "/../statlib/DBsLogins/" . $DBFile . ".php";
-	  $DebugInfo["DBErrorsFilename"] = $DBErrorsFilename;
-	  $DebugInfo["Flag"] = $debug;
-	 	$this->queries($databasename, $databaseserver, $databaseport, $databaseuser, $databasepassword, $sslkeys, $DebugInfo);
+	function __construct($debug = 0, $DBFile = "DB_OutragedDems") {
+    require $_SERVER["DOCUMENT_ROOT"] . "/../statlib/DBsLogins/" . $DBFile . ".php";
+    $DebugInfo["DBFile"] = $DBFile;
+    $DebugInfo["DBErrorsFilename"] = $DBErrorsFilename;
+    $DebugInfo["Flag"] = $debug;
+    parent::__construct($databasename, $databaseserver, $databaseport, $databaseuser, $databasepassword, $sslkeys, $DebugInfo);
   }
-  
+
   function listvendors() {
 		return $this->_return_multiple("SELECT * FROM Vendor WHERE Vendor_Stage = 'visible' AND Vendor_Visible = 'yes' ORDER BY Vendor_Order");
 	}
